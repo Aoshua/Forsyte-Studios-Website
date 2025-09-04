a4691311914c71718641c43a55d93cd92f0340c4
Josh Abbott <39091681+Aoshua@users.noreply.github.com>
2025-09-04 15:48:54 -0600

    Added background image and mission statement

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,23 @@
 <script setup lang="ts"></script>
 
 <template>
-	<div class="bg-amber-600">Hey</div>
+	<div class="h-dvh w-dvw bg-[url('/img/wallpaper.jpg')] bg-cover bg-center text-white flex justify-center">
+		<div class="space-y-10">
+			<div class="text-4xl lg:text-6xl font-semibold mt-[4rem]">Forsyte Studios</div>
+			<div>[carrousel here]</div>
+			<div class="space-y-2">
+				<div class="text-3xl">Our Mission</div>
+				<p class="text-xl max-w-[40rem]">
+					We create imaginative, accessible digital experiences that bring people together through play. We focus on crafting polished, innovative games and apps — from
+					classic platformers to unique real-world AR adventures — with the goal of sharing creativity and inspiring connection across devices and communities.
+				</p>
+			</div>
+			<div class="space-y-2">
+				<div class="text-3xl">Contact Us</div>
+				<p class="text-xl">support@forsyte-studios.com</p>
+			</div>
+		</div>
+	</div>
 </template>
 
 <style scoped></style>
