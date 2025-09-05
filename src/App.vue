8e9d9c9ff2588818054f9900379fb20da7e63545
Josh Abbott <39091681+Aoshua@users.noreply.github.com>
2025-09-05 11:30:16 -0600

    a couple mobile friendly changes

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@
 
 <template>
 	<div class="fixed inset-0 h-full w-full bg-[url('/img/wallpaper.jpg')] bg-cover bg-center bg-fixed z-0"></div>
-	<div class="relative z-10 min-h-screen w-full flex justify-center items-start overflow-y-auto text-white">
-		<div class="space-y-6 lg:space-y-10 bg-gray-900/60 p-3 sm:p-4 md:p-6 lg:rounded-xl lg:my-4 w-full max-w-xl sm:max-w-2xl md:max-w-3xl lg:max-w-4xl">
+	<div class="relative z-10 min-h-screen w-full flex justify-center items-start overflow-y-auto text-white bg-gray-900/60 lg:bg-gray-900/0">
+		<div class="space-y-6 lg:space-y-10 lg:bg-gray-900/60 p-3 sm:p-4 md:p-6 lg:rounded-xl lg:mt-10 w-full max-w-xl sm:max-w-2xl md:max-w-3xl lg:max-w-4xl">
 			<div class="flex justify-center">
 				<div class="flex flex-col gap-3 lg:gap-0 items-center">
 					<div class="h-20 w-20 md:h-40 md:w-40 -mb-6 -mt-3 lg:mt-0 bg-[url('/img/logo-white.png')] bg-cover bg-center"></div>
